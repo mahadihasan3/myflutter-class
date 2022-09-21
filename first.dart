eb27a99701c6250daa33f5e5270f762b349a4b5a
mahadihasan3 <mahadihasan10103@gmail.com>
2022-09-21 13:41:11 +0600

    day3

---
+++ first.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('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');
+  print("object");
+  print('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');
+  print("object");
+  print('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');
+  print("object");
+  print('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');
+  print("object");
+}
